2098caa7c560e6d48b5b7367b97b33f7386fa3ad
amit-karmakar06 <118373640+amit-karmakar06@users.noreply.github.com>
2024-08-21 19:56:30 +0530

    added

--- add_faces.py
+++ add_faces.py
@@ -1,9 +1,15 @@
 import cv2
+import pickle
+import numpy as np
+import os
 
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 faces_Data = []
+
 i = 0
+
+name = input("Enter username: ")
 
 while True:
     ret, frame = video.read()
@@ -28,3 +34,28 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
+faces_Data=np.asarray(faces_Data)
+faces_Data= faces_Data.reshape(100, -1)
+
+# names
+if 'names.pkl' not in os.listdir('data/'):
+    names=[name]*100
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+else:
+    with open('data/names.pkl', 'rb') as f:
+        names = pickle.load(f)
+    names = names+[name]*100
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+
+# faces
+if 'faces_data.pkl' not in os.listdir('data/'):
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+else:
+    with open('data/faces_data.pkl', 'rb') as f:
+        faces = pickle.load(f)
+    faces = np.append(faces, faces_Data, axis=0)
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(names, f)
