0203dd209f1a25d4a6b42767378e0e0d234b0945
amit-karmakar06 <118373640+amit-karmakar06@users.noreply.github.com>
2024-08-24 17:32:03 +0530

    modified test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import cv2
 import pickle
 import numpy as np
 import os
-
+import csv
+import time
+from datetime import datetime
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
@@ -19,6 +21,8 @@ knn.fit(FACES, LABELS)
 
 imgBackground = cv2.imread("background.png")
 
+COL_NAMES = ['NAME', 'TIME']
+
 while True:
     ret, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,17 +32,33 @@ while True:
         crop_image = frame[y:y+h, x:x+w]  
         resized_image = cv2.resize(crop_image, (50, 50)).flatten().reshape(1, -1)  
         output = knn.predict(resized_image)
+        ts=time.time()
+        date=datetime.fromtimestamp(ts).strftime("%d-%m-%y")
+        timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+        exist = os.path.isfile("Attendence/Attendence_" + date + ".csv")
         cv2.rectangle(frame, (x, y), (x+w,y+h),(0,0, 255), 1)
         cv2.rectangle(frame, (x, y), (x+w,y+h),(50,50, 255), 2)
         cv2.rectangle(frame, (x, y-40), (x+w,y+h),(50,50, 255), -1)
         cv2.putText(frame, str(output[0]),(x, y-15), cv2.FONT_HERSHEY_COMPLEX, 1,(255, 255, 255), 1)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 1)
+        attendence = [str(output [0]), str(timestamp)]
     imgBackground[162:162 + 480, 55:55 + 640] = frame
     cv2.imshow("frame", imgBackground)
     k = cv2.waitKey(1)
+    if k == ord('o'):
+        if exist:
+            with open("Attendence/Attendence_" + date + ".csv", "+a") as csvfile:
+                writer = csv.writer()
+                writer.writerow(attendence)
+            csvfile.close()
+        else:
+            with open("Attendence/Attendence_" + date + ".csv", "+a") as csvfile:
+                writer = csv.writer()
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendence)
+            csvfile.close()
     if k == ord('q'):
         break
-
 video.release()
 cv2.destroyAllWindows()
 
