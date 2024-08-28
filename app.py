b7c8caed9e71a29e0c1b471af52a2e211bf552ff
amit-karmakar06 <118373640+amit-karmakar06@users.noreply.github.com>
2024-08-28 17:13:54 +0530

    modified

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import time
+
+ts=time.time()
+date=datetime.fromtimestamp(ts).strftime("%d-%m-%y")
+timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+
+df = pd.read_csv("Attendence/Attendence_" + date + ".csv", "+a")
+
+
+st.dataframe(df.style)
